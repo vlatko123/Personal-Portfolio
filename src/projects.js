369ea14ae18ad0feb41e3db07b923b65653fee57
vlatko123 <v.bogojevski@yahoo.com>
2022-07-25 13:29:11 +0200

    Change in projects file

--- src/projects.js
+++ src/projects.js
@@ -2,7 +2,6 @@ export const projects = [
   {
     id: 0,
     name: "Shopping cart",
-    img: "shopping cart.png",
     link: "https://shopping-list-89bc2.web.app/",
     code: "https://github.com/vlatko123/shopping-list.git",
     description: "Application for online shopping builded in React. User can select from the given list of products by clicking on the button Add to cart. There are posibilities to add several products. After that user can check the products in cart in right angle. On the second screen there is sum of products which were selected and also product cards with single button remove. By clicking on remove user can undo selection from before. Button checkout is disabled because this app has no more functionalities. ",
@@ -11,7 +10,6 @@ export const projects = [
   {
     id: 1,
     name: "Expenses tracker",
-    img: "expenses.jpg",
     link: "https://expense-tracker-a1950.web.app/",
     code: "https://github.com/vlatko123/expense-tracker.git",
     description: "Application for tracking expenses. Under the section Entries user can insert expenses by entereing: amount, category, description of the expense. Finaly with click on the Add button expense will be shown in the new row. Every expense in the last column has Delete button which allows to remove already entered expense. In the Overviews section user can see entered expenses shown on the chart by category and ammount. ",
@@ -20,7 +18,6 @@ export const projects = [
   {
     id: 2,
     name: "Search app",
-    img: "search.png",
     link: "https://vlatko123.github.io/rickandmortyapp/",
     code: "https://github.com/vlatko123/rickandmortyapp.git",
     description: "Simple search application, for filtering characters from famous tv series. Using API call after starting the application several characters will be rendered. On the top of the application there is search bar where you can search for the particular character.",
@@ -29,18 +26,15 @@ export const projects = [
   {
     id: 3,
     name: "Bike shop",
-    img: "bike-shop.jpg",
     link: "https://vlatko123.github.io/BikeShop/",
     code: "https://github.com/vlatko123/Project-2.git",
-    description: "Bike shop with option to filter the bikes by the brand and gender.",
-    functionality: "Application builded in javaScript offers simple filter of the products listed on the screen by brand and gender. Also by click on the particular brand or gender will be performed counting of the products from that brand or gender.",
+    description: "Application builded in javaScript offers simple filter of the products listed on the screen by brand and gender. Also by click on the particular brand or gender will be performed counting of the products from that brand or gender.",
     technologies: "JS/HTML/CSS"
   },
 
   {
     id: 4,
     name: "Simple game",
-    img: "game.png",
     link: "https://vlatko123.github.io/simpleGame/",
     code: "https://github.com/vlatko123/simpleGame.git",
     description: "Simple game. Game in which you are playing against Monster. Both user and monster have progress bar with health. By clicking on the buttons user can make damage to the opponent. There are two types of attack: regular which takes a little from opponents health and strong attack which takes double than regular.  ",
@@ -48,12 +42,11 @@ export const projects = [
   },
   {
     id: 5,
-    name: "Simple Website app",
-    img: "website.png",
-    link: "https://vlatko123.github.io/Project-3/",
-    code: "https://github.com/vlatko123/Project-3.git",
-    description: "Simple website without any particular functionality.",
-    technologies: "HTML/CSS"
+    name: "Personal portfolio",
+    link: "https://vlatko123.github.io/personal-portfolio/",
+    code: "https://github.com/vlatko123/personal-portfolio",
+    description: "Personal portfolio builded in React 18. There are three sections: home, about and portfolio. In each of them there are information about me and my work.",
+    technologies: "React.js/react-router V6/context API/styled-components/CSS/react-bootstrap/framer-motion"
   },
   
   
